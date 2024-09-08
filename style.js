25164656774f5e9df3c6ec3f2a18bbf7783859bf
Manish Kumar <manish123hello@gmail.com>
2024-09-08 23:54:36 +0530

    handle restart game function

--- style.js
+++ style.js
@@ -74,6 +74,11 @@ function handleCellClick(clickedCellEvent) {
     handleResultValidation();
 }
 function handleRestartGame() {
+    gameActive = true;
+    currentPlayer = "X";
+    gameState = ["","","","","","","","",""];
+    statusDisplay.innerHTML = currentPlayerTurn();
+    document.querySelectorAll('.cell').forEach(cell => cell.innerHTML = "");
 
 }
 
