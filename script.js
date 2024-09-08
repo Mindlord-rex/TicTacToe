96779e022b4f4dfb95f3dca7d7a33069d6f90de4
Manish Kumar <manish123hello@gmail.com>
2024-09-09 00:09:57 +0530

    Refactor game logic and UI code: Update winningMessage and currentPlayerTurn functions

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ const statusDisplay = document.querySelector('.game--status');
 let gameActive = true;
 let currentPlayer = "X";
 let gameState = ["","","","","","","","",""]
-const winningMessage = () => 'Player ${currentPlayer} has won!';
+const winningMessage = () => `Player ${currentPlayer} has won!`;
 const drawMessage = () => 'Game ended in a draw!';
-const currentPlayerTurn = () => 'It`s ${currentPlayer}`s turn';
+const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
 
 statusDisplay.innerHTML = currentPlayerTurn();
 function handleCellPlayer(clickedCell, clickedCellIndex) {
